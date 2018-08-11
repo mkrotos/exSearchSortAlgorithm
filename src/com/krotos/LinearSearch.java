5b87535a612a957455fc395bd37e4f4f997e9143
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-11 12:52:26 +0200

    linear search for lists added

--- src/com/krotos/LinearSearch.java
+++ src/com/krotos/LinearSearch.java
@@ -5,7 +5,12 @@ import java.util.List;
 public class LinearSearch implements SearchAlgorithm {
     @Override
     public Integer search(Integer value, List<Integer> input) {
-        return null;
+        for(int i=0;i<input.size();i++){
+            if (input.get(i)==value){
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override

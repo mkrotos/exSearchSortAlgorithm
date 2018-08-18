306888a07b0a3746d81ebef58f3175880319edc7
Mariusz Krotos <mar.krotos@gmail.com>
2018-08-18 15:57:48 +0200

    heap sort

--- src/com/krotos/HeapSort.java
+++ src/com/krotos/HeapSort.java
@@ -10,6 +10,37 @@ public class HeapSort implements SortingAlgorithm {
 
     @Override
     public Integer[] sort(Integer[] input) {
+        budowaKopca(input);
+        rozbieranieKopca(input);
+        return input;
+    }
+
+    private Integer[] rozbieranieKopca(Integer[] input) {
+        for (int i = input.length - 1; i > 0; i--) {
+            int temp = input[0];
+            input[0] = input[i];
+            input[i] = temp;
+            int fatherInd = 0;
+            int sonInd = 1;
+            while (sonInd < i) {
+                int biggerSonInd = sonInd;
+                if (sonInd + 1 < i && input[sonInd + 1] > input[sonInd]) {
+                    biggerSonInd = sonInd + 1;
+                }
+                if (input[biggerSonInd] <= input[fatherInd]) {
+                    break;
+                }
+                temp = input[fatherInd];
+                input[fatherInd] = input[biggerSonInd];
+                input[biggerSonInd] = temp;
+                fatherInd = biggerSonInd;
+                sonInd = 2 * fatherInd + 1;
+            }
+        }
+        return input;
+    }
+
+    private Integer[] budowaKopca(Integer[] input) {
         for (int i = 1; i < input.length; i++) {
             int j = i;
             int k = getFatherInd(j);
